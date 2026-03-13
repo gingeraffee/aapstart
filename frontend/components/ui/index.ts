7051b10251659196934543679a72046ae97a92e2
Nicole <nicole.thornton330@gmail.com>
2026-03-13 17:40:56 -0500

    claude cleanup

--- frontend/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button }         from "./Button";
-export { Card }           from "./Card";
-export { Badge }          from "./Badge";
-export { ProgressDots }   from "./ProgressDots";
-export { Callout }        from "./Callout";
-export { TabGroup }       from "./TabGroup";
-export { ChecklistItem }  from "./ChecklistItem";
-export { Spinner, FullPageSpinner } from "./Spinner";
